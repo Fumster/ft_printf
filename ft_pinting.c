b54cc93696a6b84fb30c133b8e8d4f7dfd8f879a
Freely Chrystal <fchrysta@pr-j2.kzn.21-school.ru>
2021-12-01 21:29:13 +0300

    a

--- ft_pinting.c
+++ ft_pinting.c
@@ -1,39 +1,59 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_pinting.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fchrysta <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/23 21:37:01 by fchrysta          #+#    #+#             */
-/*   Updated: 2021/12/01 21:28:50 by fchrysta         ###   ########.fr       */
+/*   Created: 2021/12/01 21:20:29 by fchrysta          #+#    #+#             */
+/*   Updated: 2021/12/01 21:28:43 by fchrysta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#include "ft_printf.h"
 
-# include <stdarg.h>
-# include <unistd.h>
-
-typedef struct s_list
-{
-	int		width;
-	int		precision;
-	char	specificator[1];
-
-}	s_params;
-
-int		ft_printf(const char *p_string, ...);
-int		print_to(char *str, int *prnt_cntr, char end);
-int		fill_list(s_params *s_info, char *str);
 void    print_c(va_list args, s_list *s_info, int *prnt_cntr);
+{	
+	return ;
+}
+
 void    print_s(va_list args, s_list *s_info, int *prnt_cntr);
+
+{
+	return ;
+}
+
 void    print_p(va_list args, s_list *s_info, int *prnt_cntr);
+
+{
+	return ;
+}
+
 void    print_d(va_list args, s_list *s_info, int *prnt_cntr);
+{
+	return ;
+}
+
 void    print_i(va_list args, s_list *s_info, int *prnt_cntr);
+{
+	return ;
+}
 void    print_u(va_list args, s_list *s_info, int *prnt_cntr);
+{
+	return ;
+}
+
 void    print_x(va_list args, s_list *s_info, int *prnt_cntr);
+{
+	return ;
+}
+
 void    print_X(va_list args, s_list *s_info, int *prnt_cntr);
+{
+	return ;
+}
+
 void    print_%(va_list args, s_list *s_info, int *prnt_cntr);
-#endif
+{
+	return ;
+}
--- ft_printf.c
+++ ft_printf.c
@@ -6,17 +6,65 @@
 /*   By: fchrysta <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 21:28:00 by fchrysta          #+#    #+#             */
-/*   Updated: 2021/11/30 21:49:15 by fchrysta         ###   ########.fr       */
+/*   Updated: 2021/12/01 21:28:47 by fchrysta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int print_to_pcnt(char *str, int *prnt_cnt)
+void	print_args(va_list args, s_list *s_info, int *prnt_cntr)
+{
+	if (s_info->specificator[0] == 'c')
+		print_c(args, s_info, prnt_cntr);
+	if (s_info->specificator[0] == 's')
+		print_s(args, s_info, prnt_cntr);
+	if (s_info->specificator[0] == 'p')
+		print_p(args, s_info, prnt_cntr);
+	if (s_info->specificator[0] == 'd')
+		print_d(args, s_info, prnt_cntr);
+	if (s_info->specificator[0] == 'i')
+		print_i(args, s_info, prnt_cntr);
+	if (s_info->specificator[0] == 'u')
+		print_u(args, s_info, prnt_cntr);
+	if (s_info->specificator[0] == 'x')
+		print_x(args, s_info, prnt_cntr);
+	if (s_info->specificator[0] == 'X')
+		print_X(args, s_info, prnt_cntr);
+	if (s_info->specificator[0] == '%')
+		print_prcnt(args, s_info, prnt_cntr);
+}
+
+int	fill_list(s_params *s_info, char *str)
+{
+	int	i;
+
+	i = 1;
+	info->width = 0;
+	info->precision = 0;
+	info->specificator = 0;
+	if (!str[0])
+		return (0);
+	while ((str[i] >= '0') && (str[i] <= '9'))
+	{
+		info->width = (info->width * 10) + (str[i] - 48);
+		i++;
+	}
+	if (str[i] == '.')
+		i++;
+	while ((str[i] >= '0') && (str[i] <= '9'))
+	{
+		info->precision = (info->precision * 10) + (str[i] - 48);
+		i++;
+	}
+	info->specificator[0] = str[i];
+	return (i + 1);
+}
+
+int print_to(char *str, int *prnt_cntr, char end)
 {
 	int	i;
 	i = 0;
-	while (str[i] && (str[i] != '%'))
+	while (str[i] && (str[i] != end))
 	{
 		write(1, &str[i], 1);
 		i++;
@@ -28,8 +76,8 @@ int print_to_pcnt(char *str, int *prnt_cnt)
 int	ft_printf(const char *p_string, ...)
 {
 	int			brkpnt;
-	int			prnt_cnt;
-	s_params	info;
+	int			prnt_cntr;
+	s_params	s_info;
 	va_list		args;
 
 	pcnt_pos = 0;
@@ -38,9 +86,9 @@ int	ft_printf(const char *p_string, ...)
 	//va_arg(agrs, int) - take next argument by its type
 	while (p_string[brkpnt])
 	{
-		brkpnt += print_to_pcnt(&p_string[brkpnt], &prnt_cnt);
-		brkpnt += fill_list(info, &p_string[brkpnt]);
-		print_variable(args, &prnt_cnt)
+		brkpnt += print_to(&p_string[brkpnt], &prnt_cntr, '%');
+		brkpnt += fill_list(&s_info, &p_string[brkpnt]);
+		print_args(args, &s_info, &prnt_cntr)
 	}
 	va_end(args);
 	return (prnt_cnt);
