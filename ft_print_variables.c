9754ca282ef89feb51b24cb4acbfa97b12f6e9e1
fchrysta <fchrysta@student.21-school.ru>
2021-12-06 22:28:52 +0300

    a

--- ft_print_variables.c
+++ ft_print_variables.c
@@ -6,33 +6,58 @@
 /*   By: fchrysta <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 21:20:29 by fchrysta          #+#    #+#             */
-/*   Updated: 2021/12/06 20:41:48 by fchrysta         ###   ########.fr       */
+/*   Updated: 2021/12/06 22:28:13 by fchrysta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	print_hex(unsigned int num, int prnt_cntr, char param)
+void	create_hex_str(unsigned long int num, char *str, char param)
 {
 	int	i;
 	int	digit;
-	char	str[9];
 	
-	i = 8;
-	str[9] = 0;
+	i = 11;
+	str[12] = 0;
 	while (i >= 0)
 	{
-		num %=16;
-		digit = num;
-		if (digit < 9)
+		digit = num % 16;
+		num /= 16;
+		if (digit <= 9)
 			str[i] = digit + '0';
-		else if param = 'X'
+		else if (param == 'X')
 			str[i] = (digit - 10) + 'A';
 		else str[i] = (digit - 10) + 'a';
 		i--;
 	}
 }
 
+void    print_hex(va_list args, s_params *s_info, int *prnt_cntr, char param)
+{
+	int i;
+	char str[13];
+	unsigned long int	argument;
+
+	i = 0;
+	argument = va_arg(args, unsigned long int);
+	create_hex_str(argument, str, param);
+	if (param != 'p')
+	{
+		while (str[i] == '0')
+		i++;
+		if (!str[i])
+		{
+			write (1, "0", 1);
+			*prnt_cntr += 1;
+		}
+	}
+	while (str[i])
+	{
+		write (1, &str[i], 1);
+		*prnt_cntr += 1;
+		i++;
+	}
+}
 
 void    print_c(va_list args, s_params *s_info, int *prnt_cntr)
 {	
@@ -56,11 +81,10 @@ void    print_s(va_list args, s_params *s_info, int *prnt_cntr)
 
 void    print_p(va_list args, s_params *s_info, int *prnt_cntr)
 
-{
-	unsigned int	argument;
-
-	argument = va_arg(args, unsigned int);
-	print_hex(argument, prnt_cntr, 'p');
+{	
+	write (1, "0x", 2);
+	*prnt_cntr += 2;
+	print_hex(args, s_info, prnt_cntr, 'p');
 	return ;
 }
 
@@ -80,23 +104,16 @@ void    print_u(va_list args, s_params *s_info, int *prnt_cntr)
 
 void    print_x(va_list args, s_params *s_info, int *prnt_cntr)
 {
-	unsigned int	argument;
-
-	argument = va_arg(args, unsigned int);
-	print_hex(argument, prnt_cntr, 'x');
-	return ;
+	print_hex(args, s_info, prnt_cntr, 'x');
 }
 
 void    print_X(va_list args, s_params *s_info, int *prnt_cntr)
 {
-	unsigned int	argument;
-
-	argument = va_arg(args, unsigned int);
-	print_hex(argument, prnt_cntr, 'X');
-	return ;
+	print_hex(args, s_info, prnt_cntr, 'X');
 }
 
 void    print_prcnt(va_list args, s_params *s_info, int *prnt_cntr)
 {
-	return ;
+	write (1, "%", 1);
+	prnt_cntr += 1;	
 }
